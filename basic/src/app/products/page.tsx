87d5563285355d720101f01bd53b78f7bc235424
kot8585 <ansgywjd321@gmail.com>
2023-03-28 13:28:43 +0900

    fetch에서 SSR, ISR, CSR 해보기
    - SSR : revalidate: 0으로 설정하거나 cache: 'no store'로 지정하기
    - ISR : revalidate 시간 설정하기
    - CSR : 'use client'사용하기

--- basic/src/app/products/page.tsx
+++ basic/src/app/products/page.tsx
@@ -1,8 +1,11 @@
+import MeowArticle from "@/components/MeowArticle";
 import { getProducts } from "@/service/product";
 import Link from "next/link";
 
+export const revalidate = 3;
 export default async function ProductsPage() {
   const products = await getProducts();
+
   return (
     <>
       <h1>제품 소개 페이지!</h1>
@@ -13,6 +16,7 @@ export default async function ProductsPage() {
           </li>
         ))}
       </ul>
+      <MeowArticle />
     </>
   );
 }
